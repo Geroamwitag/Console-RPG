ce33ea931143cf8ee0e2cc73f4e79f6acb5fe0fd
Geroam <speedjunky046@gmail.com>
2024-11-10 10:37:56 +0100

    organised program functions

--- Program.cs
+++ Program.cs
@@ -8,57 +8,18 @@ class Program
     static void Main(string[] args) {
 
         // start game
-        RunGame();
-
-
-        
-        
+        RunGame();        
     }
 
+
+
+    // Main functions
     static void RunGame() {
         // run the game
         int Option = ShowMainMenu();
         Run(Option);
     }
-    
-    // generic functions
-    static int ShowMainMenu()
-    {
-        while (true) {
 
-            // gui print
-            Console.Clear();
-            Console.WriteLine("=== Very Cool RPG Game ===");
-            Console.WriteLine("1. New Game");
-            Console.WriteLine("2. Load Game");
-            Console.WriteLine("3. Options");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("=====================");
-
-            // user input
-            Console.Write("Choose an option: ");
-            string? userInnput = Console.ReadLine();
-
-            // input check
-            try {
-                int intInput = Convert.ToInt32(userInnput);
-
-                List<int> ValidOptions = new List<int> {1, 2, 3, 4};
-                if (ValidOptions.Contains(intInput)) {
-                    return intInput;
-                }
-                else {
-                    Console.WriteLine("Please choose a valid option");
-                    return ShowMainMenu();
-                }
-            }
-            catch (FormatException) {
-                return ShowMainMenu();
-            };
-
-        }
-        
-    }
 
     static void Run(int choice) {
         switch (choice)
@@ -138,6 +99,206 @@ class Program
         }
     }
 
+
+
+    // Generic functions
+    static void SaveCharacterData(Character character, int saveslot) {
+        string save_path = $"./saves/saveslot_{saveslot}.txt";
+
+        // asign save slot to character
+        character.SaveSlot = saveslot;
+
+        // write data to text file
+        using (StreamWriter writer = new StreamWriter(save_path, append: false))
+        {
+            writer.WriteLine(character.Name);
+            writer.WriteLine(character.EquipedWeapon.GetName());
+            writer.WriteLine(character.Atk - character.EquipedWeapon.Atk);
+            writer.WriteLine(character.Def);
+            writer.WriteLine(character.SkillPoints);
+            writer.WriteLine(character.ExpPoints);
+            writer.WriteLine(character.SaveSlot);
+        }
+
+        // success message
+        Console.WriteLine("Data Saved");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+
+    static void RequestSave(Character character) {
+        Console.Clear();
+        Console.Write("Save char? (y/n) ");
+        string? option = Console.ReadLine();
+        switch (option) {
+            case "y":
+                SaveCharacterData(character, character.SaveSlot);
+                break;
+            case "n":
+                Console.WriteLine("Data not saved");
+                Console.ReadKey();
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("input a valid option.");
+                Console.ReadKey();
+                RequestSave(character);
+                break;
+        }
+    }
+
+
+    static Character LoadCharacterData(string SaveFilePath) {    
+        string[] characterData = File.ReadAllLines(SaveFilePath);
+        string charName = characterData[0];
+        string weaponName = characterData[1];
+        int charAtk = int.Parse(characterData[2]);
+        int charDef = int.Parse(characterData[3]);
+        int charSP = int.Parse(characterData[4]);
+        int charExp = int.Parse(characterData[5]);
+        int charSaveSlot = int.Parse(characterData[6]);
+
+        // rebuild character
+        Character character = new Character(charName, 100, charAtk, charDef, charSP);
+
+        // add exp
+        character.ExpPoints += charExp;
+
+        // re-equip equiped weapon
+        List<Weapon> Weapons = GetWeapons();
+        foreach (var Weapon in Weapons) {
+            if (Weapon.GetName() == weaponName) {
+                int weaponIndx = Weapons.IndexOf(Weapon);
+                character.EquipWeapon(Weapons[weaponIndx]);
+            }
+        }
+
+        return character;
+    }
+
+
+    static bool CheckIntInput(string input, int max, int min) {
+        try {
+            int intinput = Convert.ToInt32(input);
+            if (intinput >= min && intinput <= max) {
+                return true;
+            }
+            return false;
+        }
+        catch (FormatException) {
+            return false;
+        }
+    }
+
+
+    static void DisplayCharacterStats(Character character) {
+        Console.WriteLine(
+                    $"""
+                    --
+                    | Character stats:                                    
+                    --
+                    | player name:    {character.Name}                    
+                    | equiped weapon: {character.EquipedWeapon.GetName()} 
+                    | Atk:            {character.Atk}                     
+                    | Def:            {character.Def}                     
+                    | SP:             {character.SkillPoints}
+                    --             
+                    """
+                );
+    }
+
+
+
+    // UI functions
+    static int ShowMainMenu()
+    {
+        while (true) {
+
+            // gui print
+            Console.Clear();
+            Console.WriteLine("=== Very Cool RPG Game ===");
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Load Game");
+            Console.WriteLine("3. Options");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("=====================");
+
+            // user input
+            Console.Write("Choose an option: ");
+            string? userInnput = Console.ReadLine();
+
+            // input check
+            try {
+                int intInput = Convert.ToInt32(userInnput);
+
+                List<int> ValidOptions = new List<int> {1, 2, 3, 4};
+                if (ValidOptions.Contains(intInput)) {
+                    return intInput;
+                }
+                else {
+                    Console.WriteLine("Please choose a valid option");
+                    return ShowMainMenu();
+                }
+            }
+            catch (FormatException) {
+                return ShowMainMenu();
+            };
+
+        }
+        
+    }
+
+
+    static int StartGameMenu() {
+        while (true) {
+                // clear UI
+            Console.Clear();
+
+            // Ui's
+            string GameMenuUI =
+            """ 
+            ===
+            Game Menu
+            1- Level Select
+            2- Inventory
+            ===
+            """;
+            
+
+            // display main menu
+            Console.WriteLine(GameMenuUI);
+            Console.Write("select an option: ");
+            string? GameMenuOption = Console.ReadLine();
+            bool validMainOption = CheckIntInput(GameMenuOption, 2, 1);
+
+            if (validMainOption) {
+                int intOption = int.Parse(GameMenuOption);
+
+                switch (intOption) {
+                    case 1:
+                        // return level select option
+                        return 1;
+                        
+                    case 2:
+                        // Inventory
+                        return 2;
+
+                    default:
+                        // error message
+                        Console.Clear();
+                        Console.WriteLine("Something went wrong");
+                        return StartGameMenu();
+                        
+                    
+                }
+            }
+        }
+        
+
+    }
+
+
     static int ShowLoadData() {
         // clear ui
         Console.Clear();
@@ -177,6 +338,41 @@ class Program
         }
     }
 
+
+    static int LevelSelect() {
+        Console.Clear();
+        string LevelSelectUi = 
+            """
+            ==
+            Level Select
+            1 - Level 1, Bandit Camp
+            ==
+            """;
+        
+        // display UI an request option
+        while (true) {
+            Console.Clear();
+            Console.WriteLine(LevelSelectUi);
+            Console.Write("select a level: ");
+            string? levelOption = Console.ReadLine();
+            bool validLevelOption = CheckIntInput(levelOption, 2, 1);
+
+            if (validLevelOption) {
+                int selectedLevel = int.Parse(levelOption);
+                Console.Clear();
+                return selectedLevel;
+            }
+            else {
+                Console.WriteLine("Please select a valid option");
+                Console.ReadKey();
+            }
+        }
+        
+    }
+
+
+
+    // Character creator functions
     static Character CreateCharacter() {
         Console.Clear();
         Console.WriteLine("=== Create Your Character ===");
@@ -210,42 +406,40 @@ class Program
         return player;
     }
 
-    static void RequestSave(Character character) {
-        Console.Clear();
-        Console.Write("Save char? (y/n) ");
-        string? option = Console.ReadLine();
-        switch (option) {
-            case "y":
-                SaveCharacterData(character, character.SaveSlot);
-                break;
-            case "n":
-                Console.WriteLine("Data not saved");
-                Console.ReadKey();
-                break;
-            default:
-                Console.Clear();
-                Console.WriteLine("input a valid option.");
-                Console.ReadKey();
-                RequestSave(character);
-                break;
-        }
-    }
 
-    static void DisplayCharacterStats(Character character) {
-        Console.WriteLine(
-                    $"""
-                    --
-                    | Character stats:                                    
-                    --
-                    | player name:    {character.Name}                    
-                    | equiped weapon: {character.EquipedWeapon.GetName()} 
-                    | Atk:            {character.Atk}                     
-                    | Def:            {character.Def}                     
-                    | SP:             {character.SkillPoints}
-                    --             
-                    """
-                );
+    static Weapon StartingWeapon(List<Weapon> weaponsList) {
+    Console.Clear();
+
+    Console.WriteLine(
+        $"""
+        === Starting equpiment ===
+        1- Barefists  :  Pure masculin fists, Atk = 0
+        2- Short Sword:  A basic short sword, Atk = 5
+        3- Great Sword:  A basic great sword, Atk = 10
+        """
+    );
+    
+    // select a weapon
+    Console.Write("Select a starting weapon: ");
+    string? option = Console.ReadLine();
+    bool intbool = CheckIntInput(option, 3, 1);
+
+    if (intbool) {
+        int intOption = Convert.ToInt32(option);
+        int weaponIDNX = intOption - 1;
+        Weapon startingWeapon = weaponsList[weaponIDNX];
+        Console.Clear();
+        Console.WriteLine($"Starting weapon selected: {startingWeapon.GetName()}\npress Enter to continue");
+        Console.ReadKey();
+        Console.Clear();
+        return startingWeapon;
     }
+    Console.Clear();
+    return StartingWeapon(weaponsList);
+
+    
+}
+
 
     static Stats AllocateStats(int availableStatPoints) {
         //reset gui
@@ -362,21 +556,41 @@ class Program
     
     }
 
-    static bool CheckIntInput(string input, int max, int min) {
-        try {
-            int intinput = Convert.ToInt32(input);
-            if (intinput >= min && intinput <= max) {
-                return true;
-            }
-            return false;
-        }
-        catch (FormatException) {
-            return false;
-        }
+
+
+    // Generation functions
+    static void RunBanditLevel(Character player) {
+        List<Enemy> BanditEnemies = GetBanditEnemies();
+        Level BanditLevel = new Level(10, BanditEnemies[0], BanditEnemies[1], BanditEnemies[2]);
+        BanditLevel.StartBattle(player, BanditEnemies[0]);
     }
-    
-    // weapon methods
-   static List<Weapon> GetWeapons() {
+
+
+    static List<Enemy> GetBanditEnemies() {
+        /*
+        0 -> reg enemy
+        1 -> mini boss enemy
+        2 -> boss enemy
+        */
+
+        // define enemy objects
+        Enemy bandit = new Enemy("Bandit",20, 2, 2);
+        Enemy banditCaptian = new Enemy("Bandit Captian",50,10,5);
+        Enemy banditLeader = new Enemy("Bandit Leader",70,12,7);
+
+        // add weapon objects to weapon list
+        List<Enemy> banditEnemyList = new List<Enemy> {
+            bandit,
+            banditCaptian,
+            banditLeader
+            };
+
+        return banditEnemyList;
+        
+    }
+
+
+    static List<Weapon> GetWeapons() {
         /*
         0 -> weak weapon
         1 -> average weapon
@@ -399,201 +613,9 @@ class Program
         
    }
 
-   static List<Enemy> GetBanditEnemies() {
-        /*
-        0 -> reg enemy
-        1 -> mini boss enemy
-        2 -> boss enemy
-        */
 
-        // define enemy objects
-        Enemy bandit = new Enemy("Bandit",20, 2, 2);
-        Enemy banditCaptian = new Enemy("Bandit Captian",50,10,5);
-        Enemy banditLeader = new Enemy("Bandit Leader",70,12,7);
 
-        // add weapon objects to weapon list
-        List<Enemy> banditEnemyList = new List<Enemy> {
-            bandit,
-            banditCaptian,
-            banditLeader
-            };
-
-        return banditEnemyList;
-        
-   }
-
-    static Weapon StartingWeapon(List<Weapon> weaponsList) {
-    Console.Clear();
-
-    Console.WriteLine(
-        $"""
-        === Starting equpiment ===
-        1- Barefists  :  Pure masculin fists, Atk = 0
-        2- Short Sword:  A basic short sword, Atk = 5
-        3- Great Sword:  A basic great sword, Atk = 10
-        """
-    );
-    
-    // select a weapon
-    Console.Write("Select a starting weapon: ");
-    string? option = Console.ReadLine();
-    bool intbool = CheckIntInput(option, 3, 1);
-
-    if (intbool) {
-        int intOption = Convert.ToInt32(option);
-        int weaponIDNX = intOption - 1;
-        Weapon startingWeapon = weaponsList[weaponIDNX];
-        Console.Clear();
-        Console.WriteLine($"Starting weapon selected: {startingWeapon.GetName()}\npress Enter to continue");
-        Console.ReadKey();
-        Console.Clear();
-        return startingWeapon;
-    }
-    Console.Clear();
-    return StartingWeapon(weaponsList);
-
-    
 }
-
-    static void SaveCharacterData(Character character, int saveslot) {
-        string save_path = $"./saves/saveslot_{saveslot}.txt";
-
-        // asign save slot to character
-        character.SaveSlot = saveslot;
-
-        // write data to text file
-        using (StreamWriter writer = new StreamWriter(save_path, append: false))
-        {
-            writer.WriteLine(character.Name);
-            writer.WriteLine(character.EquipedWeapon.GetName());
-            writer.WriteLine(character.Atk - character.EquipedWeapon.Atk);
-            writer.WriteLine(character.Def);
-            writer.WriteLine(character.SkillPoints);
-            writer.WriteLine(character.ExpPoints);
-            writer.WriteLine(character.SaveSlot);
-        }
-
-        // success message
-        Console.WriteLine("Data Saved");
-        Console.ReadKey();
-        Console.Clear();
-    }
-
-    static Character LoadCharacterData(string SaveFilePath) {    
-        
-        string[] characterData = File.ReadAllLines(SaveFilePath);
-        string charName = characterData[0];
-        string weaponName = characterData[1];
-        int charAtk = int.Parse(characterData[2]);
-        int charDef = int.Parse(characterData[3]);
-        int charSP = int.Parse(characterData[4]);
-        int charExp = int.Parse(characterData[5]);
-        int charSaveSlot = int.Parse(characterData[6]);
-
-        // rebuild character
-        Character character = new Character(charName, 100, charAtk, charDef, charSP);
-
-        // add exp
-        character.ExpPoints += charExp;
-
-        // re-equip equiped weapon
-        List<Weapon> Weapons = GetWeapons();
-        foreach (var Weapon in Weapons) {
-            if (Weapon.GetName() == weaponName) {
-                int weaponIndx = Weapons.IndexOf(Weapon);
-                character.EquipWeapon(Weapons[weaponIndx]);
-            }
-        }
-
-        return character;
-    }
-
-    static int StartGameMenu() {
-        while (true) {
-                // clear UI
-            Console.Clear();
-
-            // Ui's
-            string MainMenuUI =
-            """ 
-            ===
-            Main Menu
-            1- Level Select
-            2- Inventory
-            ===
-            """;
-            
-
-            // display main menu
-            Console.WriteLine(MainMenuUI);
-            Console.Write("select an option: ");
-            string? mainOption = Console.ReadLine();
-            bool validMainOption = CheckIntInput(mainOption, 2, 1);
-
-            if (validMainOption) {
-                int intOption = int.Parse(mainOption);
-
-                switch (intOption) {
-                    case 1:
-                        // return level select option
-                        return 1;
-                        
-                    case 2:
-                        // Inventory
-                        return 2;
-
-                    default:
-                        // error message
-                        Console.Clear();
-                        Console.WriteLine("Something went wrong");
-                        return StartGameMenu();
-                        
-                    
-                }
-            }
-        }
-        
-
-    }
-
-    static int LevelSelect() {
-        Console.Clear();
-        string LevelSelectUi = 
-            """
-            ==
-            Level Select
-            1 - Level 1, Bandit Camp
-            ==
-            """;
-        
-        // display UI an request option
-        while (true) {
-            Console.Clear();
-            Console.WriteLine(LevelSelectUi);
-            Console.Write("select a level: ");
-            string? levelOption = Console.ReadLine();
-            bool validLevelOption = CheckIntInput(levelOption, 2, 1);
-
-            if (validLevelOption) {
-                int selectedLevel = int.Parse(levelOption);
-                Console.Clear();
-                return selectedLevel;
-            }
-            else {
-                Console.WriteLine("Please select a valid option");
-                Console.ReadKey();
-            }
-        }
-        
-    }
-
-    static void RunBanditLevel(Character player) {
-        List<Enemy> BanditEnemies = GetBanditEnemies();
-        Level BanditLevel = new Level(10, BanditEnemies[0], BanditEnemies[1], BanditEnemies[2]);
-        BanditLevel.StartBattle(player, BanditEnemies[0]);
-    }
-}
-
 
 
 // generic classes
